7b48a6a0eac68f48534da369b850975207a23570
kohie-miki-im8 <kohei.miki@ai-mate.co.jp>
2026-01-02 14:41:20 +0900

    Re-enable nixos-vscode-server on WSL

--- nix/hosts/wsl/default.nix
+++ nix/hosts/wsl/default.nix
@@ -1,10 +1,19 @@
-{ config, pkgs, ... }:
+{ config, inputs, pkgs, ... }:
 {
   imports = [
     ../../modules/host
     ../../modules/wsl
     ./configuration.nix
+    inputs.nixos-vscode-server.nixosModules.default
   ];
 
   users.users.nixos.shell = pkgs.zsh;
+
+  services.vscode-server = {
+    enable = true;
+    installPath = [
+      "$HOME/.vscode-server"
+      "$HOME/.vscode-server-insiders"
+    ];
+  };
 }
