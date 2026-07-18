8f21783f1b1ba139255d6a38669b91d71fd97cae
rurusasu <36875744+rurusasu@users.noreply.github.com>
2026-07-19 07:47:36 +0900

    chore: apply gwq nixpkgs migration

--- nix/flakes/systems.nix
+++ nix/flakes/systems.nix
@@ -1,4 +1,5 @@
 { inputs, ... }:
 {
-  systems = import inputs.systems;
+  # Current nixpkgs unstable has dropped x86_64-darwin; this repository supports Apple Silicon macOS.
+  systems = builtins.filter (system: system != "x86_64-darwin") (import inputs.systems);
 }
